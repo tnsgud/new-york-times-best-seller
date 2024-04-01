199551ade1f1e9c10685d895f967bf4a432ac0f0
tnsgud <>
2024-04-01 13:40:21 +0900

    add buy link

--- app/lists/[id]/page.tsx
+++ app/lists/[id]/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { BASE_URL } from '@/index';
+import Link from 'next/link';
 import { useParams } from 'next/navigation';
 
 async function getBooks(id: string) {
@@ -17,10 +18,14 @@ export default async function Page() {
     <div className='flex flex-wrap gap-10'>
       {books.map((book) => {
         return (
-          <div key={`key-${book.title}`} className='text-center text-wrap'>
+          <Link
+            key={`key-${book.title}`}
+            className='text-center text-wrap'
+            href={book.buy_links[0].url}
+          >
             <img className='w-40 h-60' src={book.book_image} />
             {book.title}
-          </div>
+          </Link>
         );
       })}
     </div>
